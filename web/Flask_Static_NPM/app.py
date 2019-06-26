2bf78018806506427d7415409e9accc47e5ae47b
mebaysan <menesbaysan@gmail.com>
2019-06-26 23:32:14 +0300

    flask ile static NPM kullanma

---
+++ web/Flask_Static_NPM/app.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html",user="Baysan")
+
+
+if __name__ == '__main__':
+    app.run()
