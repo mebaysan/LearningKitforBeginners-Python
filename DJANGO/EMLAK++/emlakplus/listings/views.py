5fd17f137e46e4e4b21701a3fc87140864b6a77a
mebaysan <menesbaysan@gmail.com>
2019-09-18 17:33:32 +0300

    added filter & pagination & ordered

--- DJANGO/EMLAK++/emlakplus/listings/views.py
+++ DJANGO/EMLAK++/emlakplus/listings/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3)  # hangi obje üzerinde pagination yapacak ve sayfa başına kaç tane gösterecek
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
-    return render(request, 'listings/listings.html',context)
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request,listing_id):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
