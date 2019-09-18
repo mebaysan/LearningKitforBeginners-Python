9737ca12230ab270f7f5921020ff58ddae21bccd
mebaysan <menesbaysan@gmail.com>
2019-09-18 18:23:45 +0300

    added about & home dynamic pages

--- DJANGO/EMLAK++/emlakplus/pages/views.py
+++ DJANGO/EMLAK++/emlakplus/pages/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
+
 
 def index(request):
-    return render(request,"pages/index.html")
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, "pages/index.html", context)
+
 
 def about(request):
-    return render(request,"pages/about.html")
+    # Get all Realtors
+    realtors = Realtor.objects.order_by('-hire_date') #hepsini aldık tutulma tarihlerine göre
+    # Get MVP
+    mvp_realtors = Realtor.objects.filter(is_mvp=True) #sadece mvp olanları aldık
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, "pages/about.html", context)
