3e879730d7d119f82eb7d2d0b87afd9bda11950c
baysan <baysan@baysan.baysan.com>
2019-09-20 00:47:52 +0300

    post model added

--- DJANGO/DjangoRestFramework/blog/post/models.py
+++ DJANGO/DjangoRestFramework/blog/post/models.py
@@ -1,4 +1,7 @@
 from django.db import models
-from django.contrib.auth import Us
+from django.contrib.auth.models import User
 # Create your models here.
 class Post:
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    content = models.TextField()
