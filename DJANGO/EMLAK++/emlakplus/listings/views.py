0604a6aa1b71aa4973930eaacd7c01f38011a637
mebaysan <menesbaysan@gmail.com>
2019-09-19 10:47:48 +0300

    search template prepared

--- DJANGO/EMLAK++/emlakplus/listings/views.py
+++ DJANGO/EMLAK++/emlakplus/listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .choices import bedroom_choices, price_choices, state_choices
 
 
 # Create your views here.
@@ -10,14 +11,23 @@ def index(request):
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
     context = {
-        'listings': paged_listings
+        'listings': paged_listings,
     }
     return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, id=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices
+    }
+    return render(request, 'listings/search.html', context)
--- DJANGO/EMLAK++/emlakplus/pages/views.py
+++ DJANGO/EMLAK++/emlakplus/pages/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from listings.models import Listing
 from realtors.models import Realtor
-
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices
     }
     return render(request, "pages/index.html", context)
 
