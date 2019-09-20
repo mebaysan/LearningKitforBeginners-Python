de86ea8f190ec7b01b06bedc0af2ed8f1d0d888c
mebaysan <menesbaysan@gmail.com>
2019-09-20 11:51:55 +0300

    restframework

--- DJANGO/DjangoRestFramework/blog/post/api/urls.py
+++ DJANGO/DjangoRestFramework/blog/post/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import PostListAPIView
+from .views import PostListAPIView,PostDetailAPIView
 
 
 urlpatterns = [
     path('list', PostListAPIView.as_view(), name="list"),
+    path('detail/<slug>', PostDetailAPIView.as_view(), name="detail"),
 ]
