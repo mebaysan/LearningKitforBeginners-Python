5e79d3746a3d422275b3a74bef6bdf2d472a5c37
mebaysan <menesbaysan@gmail.com>
2019-10-02 23:27:18 +0300

    ileri seviye django temelleri icin proje kurulumu

---
+++ DJANGO/Django_Temeller_Ileri_Seviye/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\menes\desktop\learningkitforbeginners-python\django\django_temeller_ileri_seviye\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
