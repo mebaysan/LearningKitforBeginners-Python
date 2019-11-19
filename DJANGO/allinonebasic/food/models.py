b17550297bb6c44addc12082f98084c193debe06
mebaysan <menesbaysan@gmail.com>
2019-11-19 17:47:07 +0300

    end of database&models section

--- DJANGO/allinonebasic/food/models.py
+++ DJANGO/allinonebasic/food/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=200)
+    price = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
