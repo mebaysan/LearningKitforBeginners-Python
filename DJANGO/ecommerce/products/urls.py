50cf993910ca02e2b7415ef8b81f96f4be09d597
mebaysan <menesbaysan@gmail.com>
2019-11-24 01:44:48 +0300

    deleted crashes

--- DJANGO/ecommerce/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from products import views
-
-urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product_list'),
-    path('detail/<slug:slug>', views.product_detail, name='product_detail'),
-    path('featured_products', views.product_featured_list, name='product_featured_list'),
-    path('featured_products/detail/<slug:slug>', views.product_featured_detail, name='product_featured_detail'),
-]
