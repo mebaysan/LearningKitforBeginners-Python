11ff0eb9a2ca1e818afeaf6a287df27acb9e6185
mebaysan <menesbaysan@gmail.com>
2019-12-14 00:38:04 +0300

    added panel/news_list

--- DJANGO/fromzerotohero/panel/urls.py
+++ DJANGO/fromzerotohero/panel/urls.py
@@ -6,4 +6,5 @@ app_name = 'panel'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('news_list', views.news_list, name='news_list'),
 ]
--- DJANGO/fromzerotohero/panel/views.py
+++ DJANGO/fromzerotohero/panel/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from news.models import News
+
 
 # Create your views here.
 def home(request):
     return render(request, 'back/home.html')
+
+
+def news_list(request):
+    news_list = News.objects.all()
+    context = {
+        'news_list': news_list,
+    }
+    return render(request, 'back/news_list.html', context=context)
