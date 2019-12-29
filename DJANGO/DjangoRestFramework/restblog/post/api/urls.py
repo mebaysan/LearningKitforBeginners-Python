25f2d5474445ef35f7c528bdf3ffbce471a7c0f3
mebaysan <menesbaysan@gmail.com>
2019-12-29 23:26:49 +0300

    s

--- DJANGO/DjangoRestFramework/restblog/post/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-from post.api.views import PostListAPIView
-
-urlpatterns = [
-    path('list/', PostListAPIView.as_view(), name="list"),
-]
--- DJANGO/DjangoRestFramework/restblog/post/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class Post(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE) # ilk parametre ne ile foreignleyeceğimiz
-    title = models.CharField(max_length=120)
-    content = models.TextField()
