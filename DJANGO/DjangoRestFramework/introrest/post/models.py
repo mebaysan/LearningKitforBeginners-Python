dcf256cd51a4e36db3efce8131833956b724b4ca
mebaysan <menesbaysan@gmail.com>
2020-01-10 16:07:32 +0300

    start the django rest intro project

---
+++ DJANGO/DjangoRestFramework/introrest/post/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    content = models.TextField()
