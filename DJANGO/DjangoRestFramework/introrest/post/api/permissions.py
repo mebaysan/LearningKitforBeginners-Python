57bf1f587aafca9806bbcef9abfb4f8ad4ea83c3
mebaysan <menesbaysan@gmail.com>
2020-01-22 20:28:27 +0300

    added has_permission

--- DJANGO/DjangoRestFramework/introrest/post/api/permissions.py
+++ DJANGO/DjangoRestFramework/introrest/post/api/permissions.py
@@ -4,5 +4,10 @@ from rest_framework.permissions import BasePermission
 class IsOwner(BasePermission):
     message = "You must be the owner of this object."
 
+    def has_permission(self, request,
+                       view):  # has_object_permission'a girmeden önce burası çalışır ve her istek tipinde (post,get,delete..) çalışır
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user or request.user.is_superuser
+        return (
+                       obj.user == request.user) or request.user.is_superuser  # nesnenin sahibi ise True veya admin ise True dönecek
